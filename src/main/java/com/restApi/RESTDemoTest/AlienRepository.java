cf5baff3bc3a5eed9a84dfa7ca64e3b0d492a46f
snigdha <sunny.rangineni@gmail.com>
2024-06-06 16:34:18 -0500

    adding Alien data to Database on post request from postman

--- src/main/java/com/restApi/RESTDemoTest/AlienRepository.java
+++ src/main/java/com/restApi/RESTDemoTest/AlienRepository.java
@@ -1,46 +1,92 @@
 package com.restApi.RESTDemoTest;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AlienRepository {
 	List<Alien> aliens;
 	
+	Connection con = null;
+	String url = "jdbc:postgresql://localhost:5432/postgres";
+	String username = "postgres";
+	String password = "1234";
+	
 	public AlienRepository()
 	{
-		aliens = new ArrayList<>();
-		Alien a1 = new Alien();
-		a1.setId(101);
-		a1.setName("Snigdha");
-		a1.setPoints(80);
+		try {
+			Class.forName("org.postgresql.Driver");
+			con = DriverManager.getConnection(url,username,password);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		Alien a2 = new Alien();
-		a2.setId(102);
-		a2.setName("Ram");
-		a2.setPoints(75);
-		aliens.add(a1);
-		aliens.add(a2);
 	}
 	
 	public List<Alien> getAliens()
 	{
+		List<Alien> aliens = new ArrayList<Alien>();
+		String sql = "Select * from alien";
+		try {
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			while(rs.next())
+			{
+				Alien a = new Alien();
+				a.setId(rs.getInt(1));
+				a.setName(rs.getString(2));
+				a.setPoints(rs.getInt(3));
+				
+				aliens.add(a);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return aliens;
 	}
 	
 	public Alien getAlien(int id)
 	{
-		
-		for(Alien a:aliens)
-		{
-			if(a.getId()==id)
-				return a;
+		Alien a = new Alien();
+		String sql = "Select * from alien where id = " +id;
+		try {
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery(sql);
+			if(rs.next())
+			{
+				a.setId(rs.getInt(1));
+				a.setName(rs.getString(2));
+				a.setPoints(rs.getInt(3));
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		return null;
-		
+		return a;
 	}
 
 	public void create(Alien a1) {
-		// TODO Auto-generated method stub
-		aliens.add(a1);
+		String sql = "INSERT INTO alien VALUES (?, ?, ?)";
+		try {
+			PreparedStatement st = con.prepareStatement(sql);
+			st.setInt(1, a1.getId());
+			st.setString(2, a1.getName());
+			st.setInt(3, a1.getPoints());
+			st.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
